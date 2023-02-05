fde4ab9519fb90007a645bc9af277a503b416868
Gloria Ikharo <68612097+Olohi1@users.noreply.github.com>
2023-02-05 16:26:42 +0100

    Update tiktok.sql

--- tiktok.sql
+++ tiktok.sql
@@ -1,11 +1,18 @@
+/* DATA INFORMATION */
+SELECT * 
+FROM tiktok_song
+
 /* MOST POPULAR TRACK */
 SELECT MAX (track_pop) AS track_popularity, track_name
 FROM tiktok_song
 GROUP BY track_name
 ORDER BY MAX(track_pop) DESC LIMIT 1;
 
-/* MOST POPULAR ARTIST (BASED ON THE SUM OF THE 
-POPULARITY RATING OF EACH ARTIST) */
+/* TOTAL NUMBER OF ARTIST */
+SELECT COUNT (DISTINCT artist_name)
+FROM tiktok_song
+
+/* MOST POPULAR ARTIST (BASED ON THE SUM OF THE POPULARITY RATING OF EACH ARTIST) */
 SELECT SUM (artist_pop) AS artist_popularity, artist_name
 FROM tiktok_song
 GROUP BY artist_name, artist_pop
@@ -16,7 +23,8 @@ SELECT danceability, track_name, artist_name
 FROM tiktok_song
 GROUP BY track_name, artist_name, danceability
 ORDER BY danceability ASC LIMIT 1;
-/* COMPARISON OF THE MOST DANCEABLE AND LEAST DANCEALE TRACK */
+
+/* COMPARISON OF THE LEAST DANCEABLE AND MOST DANCEABLE TRACK */
 SELECT *
 FROM tiktok_song
 WHERE 
